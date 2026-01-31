d3effceea5dd6aaf39fc66a6ee60c7f81f2eff7c
ggnagamallika-jpg <ggnagamallika@gmail.com>
2026-01-31 15:47:39 +0530

    Add files via upload

---
+++ User.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class User{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter age");
+        int age=sc.nextInt();
+        System.out.println(age);
+    }
+}
