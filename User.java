0dde84be601001cc0207d2bf93dc703d14f7e820
ggnagamallika-jpg <ggnagamallika@gmail.com>
2026-01-31 16:03:46 +0530

    Add files via upload

--- User.java
+++ User.java
@@ -2,8 +2,17 @@ import java.util.*;
 public class User{
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter age");
+        System.out.println("enter age");
         int age=sc.nextInt();
+        System.out.println("enter a");
+        double a=sc.nextDouble();
+        System.out.println("enter b");
+        float b=sc.nextFloat();
+        System.out.println("enter str1");
+        String str1=sc.next();
+        String str2=sc.nextLine();
+        System.out.println(a);
+        System.out.println(b);
         System.out.println(age);
     }
 }
