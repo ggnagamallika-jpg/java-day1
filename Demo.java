a00a25dd8ef2690d3f1a2d31833d1dc9dface709
ggnagamallika-jpg <ggnagamallika@gmail.com>
2026-01-31 14:30:54 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,9 @@
+public class Demo{
+    public static void main(String args[]){
+        int a=20;
+        int b=25;
+        int c=a+b;
+        System.out.println(c);
+        System.out.print("good morning");
+    }
+}
