7206215d7a8a2ffb8faa6c2883a6e1ccbb647fe3
ggnagamallika-jpg <ggnagamallika@gmail.com>
2026-02-07 14:52:35 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,11 @@
+public class Arithmetic{
+    public static void main(String args[]){
+        int a=15;
+        int b=20;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
