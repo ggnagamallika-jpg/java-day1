bdbb3762a5cb26d0ed767c5d504c5eaf44837009
ggnagamallika-jpg <ggnagamallika@gmail.com>
2026-02-07 14:59:33 +0530

    Add files via upload

---
+++ Relational.java
@@ -0,0 +1,11 @@
+public class Unary{
+    public static void main(String args[]){
+        byte a=10
+        System.out.println(+a);
+        System.out.println(-a);
+        System.out.println(++a);
+        System.out.println(--a);
+        System.out.println(a++);
+        System.out.println(a--);
+    }
+}
