85ffe0aae7717ad9c3e2d11703b199c404829fef
ggnagamallika-jpg <ggnagamallika@gmail.com>
2026-02-07 10:53:51 +0530

    Add files via upload

---
+++ Primitive.java
@@ -0,0 +1,18 @@
+public class Primitive{
+    public static void main(String args[]){
+        byte a=120;
+        short b=9500;
+        int c=60000;
+        float d=57.54f;
+        double e=9867543;
+        char f='#';
+        boolean value=true;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(value);
+    }
+}
