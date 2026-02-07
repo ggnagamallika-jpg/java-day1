31fb80d6989d06a9d05de6c5a4891b8152100e73
ggnagamallika-jpg <ggnagamallika@gmail.com>
2026-02-07 14:59:57 +0530

    Add files via upload

---
+++ Assignment.java
@@ -0,0 +1,11 @@
+public class Assignment{
+    public static void main(String args[]){
+        int a=15;
+        int b=10;
+        System.out.println(a+=b);
+        System.out.println(a-=b);
+        System.out.println(a*=b);
+        System.out.println(a/=b);
+        System.out.println(a%=b);
+    }
+}
